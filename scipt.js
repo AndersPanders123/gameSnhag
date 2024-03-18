5f594a78e5d6717d6998519778d15bdbfe0219af
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2024-03-18 14:15:48 +0100

    snhag
    snhag

---
+++ scipt.js
@@ -0,0 +1,16 @@
+const song = new Audio('SANG.mp3')
+
+song.volume = 0.1
+
+function sPILLSANG() {
+    song.play();
+    console.log('NEGHR')
+}
+ 
+const Snhag = document.getElementById("Knristian")
+
+
+Snhag.addEventListener('click', sPILLSANG)
+
+
+
